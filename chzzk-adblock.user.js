418b330d602a0568798506e5608141c421f4649f
1st_award <54760813+1st-award@users.noreply.github.com>
2024-01-22 12:49:39 +0900

    Update chzzk-adblock.user.js

--- chzzk-adblock.user.js
+++ chzzk-adblock.user.js
@@ -1,13 +1,13 @@
 // ==UserScript==
 // @name         Chzzk Adblock
 // @namespace    http://tampermonkey.net/
-// @version      1.1
+// @version      1.0.1
 // @description  Chzzk Adblock
 // @author       1st_award
 // @match        https://chzzk.naver.com/live/*
 // @icon         data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==
-// @updateURL    https://github.com/1st-award/chzzk-adblock/raw/main/chzzk-adblock.js
-// @downloadURL  https://github.com/1st-award/chzzk-adblock/raw/main/chzzk-adblock.js
+// @updateURL    https://github.com/1st-award/chzzk-adblock/raw/main/chzzk-adblock.user.js
+// @downloadURL  https://github.com/1st-award/chzzk-adblock/raw/main/chzzk-adblock.user.js
 // @grant        none
 // ==/UserScript==
 
